47815246cb1a2a2f98ffae9517753ccbc7496c74
Campbell Lund <cl4@wellesley.edu>
2025-02-20 12:37:24 +0000

    update table

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
-fetch('data/conflicting.json')
-  .then(response => response.json())
-  .then(conflictingData => {
-    const container = document.getElementById('conflict-table');
+// fetch('data/conflicting.json')
+//   .then(response => response.json())
+//   .then(conflictingData => {
+//     const container = document.getElementById('conflict-table');
     
-    // Build an HTML table or list of conflicts
-    let html = '<table border="1"><tr><th>Term</th><th>Definition</th><th>Discipline</th></tr>';
+//     // Build an HTML table or list of conflicts
+//     let html = '<table border="1"><tr><th>Term</th><th>Definition</th><th>Discipline</th></tr>';
     
-    conflictingData.forEach(item => {
-      html += `<tr>
-                 <td>${item.term}</td>
-                 <td>${item.definition}</td>
-                 <td>${item.discipline}</td>
-               </tr>`;
-    });
+//     conflictingData.forEach(item => {
+//       html += `<tr>
+//                  <td>${item.term}</td>
+//                  <td>${item.definition}</td>
+//                  <td>${item.discipline}</td>
+//                </tr>`;
+//     });
     
-    html += '</table>';
-    container.innerHTML = html;
-  });
+//     html += '</table>';
+//     container.innerHTML = html;
+//   });
